c81d7721ab3b0170f9ff45781776ee7cfc03c3a7
Markmanus <pampadalampa@gmail.com>
2023-04-06 21:27:21 +0100

    blabla

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
+import random
 from turtle import Turtle, Screen
 
-tim = Turtle()
-sceen = Screen()
+is_race_on = False
+
+screen = Screen()
+
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which Turtle will win the race, enter the color.")
+print(user_bet)
+colors = ["red","orange","yellow","green","blue","purple"]
+y_pos = [-70,-40,-10,20,50,80]
+all_turtles = []
+
+for turtle_index in range(0,6):
+
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_pos[turtle_index])
+    new_turtle.color(colors[turtle_index])
+    all_turtles.append(new_turtle)
 
 
-sceen.exitonclick()
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
+
+
+
+
+
+screen.exitonclick()
